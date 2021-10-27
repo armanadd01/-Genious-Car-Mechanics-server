fd9c2cf83dca791307cf4ce03734cd8227e7c9fc
Arman Habib <32464369+armanadd01@users.noreply.github.com>
2021-10-28 03:27:33 +0600

    commit

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! bt');
 })
+
+app.get('/users', (req, res) => {
+    res.send('Hello World! bt');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
